aeb8da7eb317c95bd5e8ed58762026d312f7ea83
servetto.matias@gmail.com <servetto.matias@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-07 23:19:11 +0000

    [P-31] An example for the use of validation library found on spike 31

---
+++ src/ValidatorAppSample/Validator/build/source/buildConfig/debug/com/throrinstudio/android/example/validator/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.throrinstudio.android.example.validator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
---
+++ src/ValidatorAppSample/Validator/gen/com/throrinstudio/android/example/validator/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.throrinstudio.android.example.validator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
