aaec295ed35d7d0e37d919c213519b706137bce0
servetto.matias@gmail.com <servetto.matias@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-08 17:59:56 +0000

    [PT-23] Project with dependency to Validator Library

--- trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/RegisterUserActivity.java
+++ trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/RegisterUserActivity.java
@@ -1,5 +1,7 @@
 package phoneticket.android.activities;
 
+import com.throrinstudio.android.common.libs.validator.Form;
+
 import phoneticket.android.R;
 import android.os.Bundle;
 import android.app.Activity;
@@ -7,10 +9,18 @@ import android.view.View;
 
 public class RegisterUserActivity extends Activity {
 
+	private Form form;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register_user);
+		
+		createForm();
+	}
+	
+	private void createForm() {
+		form = new Form();
 	}
 
 	public void onRegisterButtonAction(View sender) {
