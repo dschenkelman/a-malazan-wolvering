ab2e20119767a168c63d2b069c0585f6224a72cf
servetto.matias@gmail.com <servetto.matias@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-08 17:29:53 +0000

    [PT 23] Register User View

---
+++ trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/RegisterUserActivity.java
@@ -0,0 +1,19 @@
+package phoneticket.android.activities;
+
+import phoneticket.android.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+
+public class RegisterUserActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register_user);
+	}
+
+	public void onRegisterButtonAction(View sender) {
+		
+	}
+}
