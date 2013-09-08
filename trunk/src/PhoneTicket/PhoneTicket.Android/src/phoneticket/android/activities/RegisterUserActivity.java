5a9f5310b24dbc32f6bec449a38e0074557a32bc
servetto.matias@gmail.com <servetto.matias@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-08 18:21:22 +0000

    [PT-23] Validate Library added correctly as android dependency. Register Form

--- trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/RegisterUserActivity.java
+++ trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/RegisterUserActivity.java
@@ -1,15 +1,24 @@
 package phoneticket.android.activities;
 
+import com.throrinstudio.android.common.libs.validator.AbstractValidate;
 import com.throrinstudio.android.common.libs.validator.Form;
+import com.throrinstudio.android.common.libs.validator.Validate;
+import com.throrinstudio.android.common.libs.validator.validate.ConfirmValidate;
+import com.throrinstudio.android.common.libs.validator.validate.OrTwoRequiredValidate;
+import com.throrinstudio.android.common.libs.validator.validator.EmailValidator;
+import com.throrinstudio.android.common.libs.validator.validator.NotEmptyValidator;
 
 import phoneticket.android.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class RegisterUserActivity extends Activity {
 
-	private Form form;
+	private Form registerForm;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +29,57 @@ public class RegisterUserActivity extends Activity {
 	}
 	
 	private void createForm() {
-		form = new Form();
+        Validate nameField              = new Validate((EditText)findViewById(R.id.inputFirstName));
+        Validate lastNameField       	= new Validate((EditText)findViewById(R.id.inputLastName));
+        Validate emailField             = new Validate((EditText)findViewById(R.id.inputEmail));
+        Validate dniField           	= new Validate((EditText)findViewById(R.id.inputDNI));
+        Validate birthdayField        	= new Validate((EditText)findViewById(R.id.inputBirthday));
+        Validate phoneField             = new Validate((EditText)findViewById(R.id.inputPhone));
+        Validate cellPhoneField         = new Validate((EditText)findViewById(R.id.inputCellPhone));
+        Validate passwordField          = new Validate((EditText)findViewById(R.id.inputPassword));
+        Validate confirmPasswordField   = new Validate((EditText)findViewById(R.id.inputConfirmPassword));
+
+        /*
+        AbstractValidate phonesFields = new OrTwoRequiredValidate(
+                (EditText)findViewById(R.id.inputPhone),
+                (EditText)findViewById(R.id.inputCellPhone));*/
+        AbstractValidate confirmFields = new ConfirmValidate(
+                (EditText)findViewById(R.id.inputPassword),
+                (EditText)findViewById(R.id.inputConfirmPassword));
+
+        Context context = getApplicationContext();
+
+        nameField.addValidator(new NotEmptyValidator(context));
+        lastNameField.addValidator(new NotEmptyValidator(context));
+        emailField.addValidator(new NotEmptyValidator(context));
+        dniField.addValidator(new NotEmptyValidator(context));
+        passwordField.addValidator(new NotEmptyValidator(context));
+        confirmPasswordField.addValidator(new NotEmptyValidator(context));
+
+        emailField.addValidator(new EmailValidator(context));
+
+        /*
+        phoneField.addValidator(new PhoneOrEmptyValidator(context));
+        cellPhoneField.addValidator(new PhoneOrEmptyValidator(context));*/
+
+        registerForm = new Form();
+        registerForm.addValidates(nameField);
+        registerForm.addValidates(lastNameField);
+        registerForm.addValidates(emailField);
+        registerForm.addValidates(dniField);
+        registerForm.addValidates(birthdayField);
+        registerForm.addValidates(phoneField);
+        registerForm.addValidates(cellPhoneField);
+        registerForm.addValidates(passwordField);
+        registerForm.addValidates(confirmPasswordField);
+        registerForm.addValidates(confirmFields);
 	}
 
 	public void onRegisterButtonAction(View sender) {
-		
+		if(registerForm.validate()) {
+			Log.d("PhoneTicket", "form is valid");
+		} else {
+			Log.d("PhoneTicket", "form is not valid");
+		}
 	}
 }
