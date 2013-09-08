36616b13831a446f4ab2f7e1598815341a327fe6
servetto.matias@gmail.com <servetto.matias@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-08 16:59:43 +0000

    [PT-23 24 and 25] XML Layouts

---
+++ trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package phoneticket.android.activities;
+
+import phoneticket.android.R;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+}
---
+++ trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/MainActivity.java
@@ -0,0 +1,22 @@
+package phoneticket.android.activities;
+
+import phoneticket.android.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void onActionButtonTapped(View sender) {
+     	Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+	}
+
+}
