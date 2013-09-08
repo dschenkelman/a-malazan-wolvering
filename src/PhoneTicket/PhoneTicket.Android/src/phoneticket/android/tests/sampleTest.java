5476d0f75dbc1822bb0b4cda73b2b47e12926e23
servetto.matias@gmail.com <servetto.matias@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-08 17:17:36 +0000

    [PT- 23, 24 and 25] Login layout. [SPIKE Tests] test sample and manifest with test.instrumentation

---
+++ src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/tests/sampleTest.java
@@ -0,0 +1,15 @@
+package phoneticket.android.tests;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class sampleTest extends TestCase {
+
+	public void setUp() {
+		
+	}
+	
+	public void testOk() {
+		Assert.assertTrue(true);
+	}
+}
