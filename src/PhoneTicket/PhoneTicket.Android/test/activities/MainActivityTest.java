0c16f398be5c60a11eb1a4e11d7b16e47aa31dc7
sebastianr213@gmail.com <sebastianr213@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-08 22:48:27 +0000

    # Initial test try

---
+++ src/PhoneTicket/PhoneTicket.Android/test/activities/MainActivityTest.java
@@ -0,0 +1,36 @@
+package activities;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import phoneticket.android.R;
+import phoneticket.android.activities.MainActivity;
+
+import android.view.View;
+import android.widget.Button;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+
+	private MainActivity activity;
+	private Button login;
+	
+	@Before
+    public void setUp() throws Exception {
+		activity = Robolectric.buildActivity(MainActivity.class).create().get();
+        login = (Button) activity.findViewById(R.id.actionButton);
+    }
+	
+	@Test
+	public void buttonShouldBeVisible() throws Exception{
+		assertThat(login.getVisibility(), equalTo(View.VISIBLE));
+	}
+	
+}
