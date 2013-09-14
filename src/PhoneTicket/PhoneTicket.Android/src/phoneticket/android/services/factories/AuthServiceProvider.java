3c477b0bc93fdcabff64c1b809d75eb2d9fc5fc7
gdfesta <gdfesta@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-14 15:16:28 +0000

    Refactor name of Service's implementation.

--- src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/services/factories/AuthServiceProvider.java
+++ src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/services/factories/AuthServiceProvider.java
@@ -1,7 +1,7 @@
 package phoneticket.android.services.factories;
 
 import phoneticket.android.services.post.IAuthService;
-import phoneticket.android.services.post.impl.AuthServiceImpl;
+import phoneticket.android.services.post.impl.AuthService;
 
 import com.google.inject.Provider;
 
@@ -9,7 +9,7 @@ public class AuthServiceProvider implements Provider<IAuthService> {
 
 	@Override
 	public IAuthService get() {
-		return new AuthServiceImpl();
+		return new AuthService();
 	}
 
 }
