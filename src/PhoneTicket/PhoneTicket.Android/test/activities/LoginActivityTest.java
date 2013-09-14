9b0115f0f1be0d0046463e1f6e754783a3b2806d
gdfesta <gdfesta@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-14 18:19:54 +0000

    LoginActivity's Test  and RegistrarActivity's Test

--- src/PhoneTicket/PhoneTicket.Android/test/activities/LoginActivityTest.java
+++ src/PhoneTicket/PhoneTicket.Android/test/activities/LoginActivityTest.java
@@ -22,33 +22,55 @@ import phoneticket.android.validator.IFormValidator;
 
 @RunWith(RobolectricTestRunner.class)
 public class LoginActivityTest {
-	 
-	@Mock private IAuthService authService;
-	@Mock private IFormValidator formValidator;
+
+	@Mock
+	private IAuthService authService;
+	@Mock
+	private IFormValidator formValidator;
 	private LoginActivity activity;
 	private Button login;
 	private EditText user;
 	private EditText password;
 
 	@Before
-	  public void setUp() {
-	    MockitoAnnotations.initMocks(this);
-	    TestModule module = new TestModule();
-	    module.addBinding(IAuthService.class, authService);
-	    module.addBinding(IFormValidator.class, formValidator);
-	    TestModule.setUp(this, module);
-		activity = Robolectric.buildActivity(LoginActivity.class).create().get();
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		TestModule module = new TestModule();
+		module.addBinding(IAuthService.class, authService);
+		module.addBinding(IFormValidator.class, formValidator);
+		TestModule.setUp(this, module);
+		activity = Robolectric.buildActivity(LoginActivity.class).create()
+				.get();
 		login = (Button) activity.findViewById(R.id.loginButton);
-		user = (EditText)activity.findViewById(R.id.inputEmail);
-		password = (EditText)activity.findViewById(R.id.inputPassword);
-	  }
+		user = (EditText) activity.findViewById(R.id.inputEmail);
+		user.setText("gfesta@gmail.com");
+		password = (EditText) activity.findViewById(R.id.inputPassword);
+		password.setText("password");
+	}
+
+	@Test
+	public void loginActivityCallFormValidator(){
+		login.performClick();
+		Mockito.verify(formValidator,Mockito.times(1)).validate();
+	}
 	
 	@Test
-	public void loginActivityCallAuthService(){
-		user.setText("gfesta@gmail.com");
-		password.setText("password");
+	public void loginActivityCallAuthService() {
 		Mockito.when(formValidator.validate()).thenReturn(true);
 		login.performClick();
-		Mockito.verify(authService,Mockito.times(1)).authUser(activity, new LoginUser(user.getText().toString(), password.getText().toString()));
+		Mockito.verify(authService, Mockito.times(1)).authUser(
+				activity,
+				new LoginUser(user.getText().toString(), password.getText()
+						.toString()));
+	}
+
+	@Test
+	public void loginActivityNotCallAuthService() {
+		Mockito.when(formValidator.validate()).thenReturn(false);
+		login.performClick();
+		Mockito.verify(authService, Mockito.times(0)).authUser(
+				activity,
+				new LoginUser(user.getText().toString(), password.getText()
+						.toString()));
 	}
 }
