c18cf78241bdf9713f4d5564aa50acb8b5c8ae75
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-14 11:53:13 +0000

    #27 Initial web project commit. Includes support for HTTPS for REST API.

---
+++ src/PhoneTicket.Web/PhoneTicket.Web/Handlers/RequireHttpsMessageHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace PhoneTicket.Web.Handlers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class RequireHttpsMessageHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            IEnumerable<string> protos;
+            request.Headers.TryGetValues("X-Forwarded-Proto", out protos);
+
+            bool wasHttps = protos != null && protos.Any(p => p.Equals("https", StringComparison.InvariantCultureIgnoreCase));
+
+            bool isHttps = request.RequestUri.Scheme == Uri.UriSchemeHttps;
+
+            if (!wasHttps && !isHttps)
+            {
+                var forbiddenResponse = request.CreateResponse(HttpStatusCode.Forbidden);
+
+                forbiddenResponse.ReasonPhrase = "SSL Required";
+                return Task.FromResult<HttpResponseMessage>(forbiddenResponse);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
