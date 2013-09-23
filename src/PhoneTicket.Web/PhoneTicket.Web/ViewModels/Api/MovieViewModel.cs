8368d7803c7916bc14515ff42f2399925e79afb4
sebastianr213@gmail.com <sebastianr213@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-23 02:57:49 +0000

    [PT-123 Implementar endpoint para obtener detalles de una película (servidor)] Completed

---
+++ src/PhoneTicket.Web/PhoneTicket.Web/ViewModels/Api/MovieViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneTicket.Web.ViewModels.Api
+{
+    public class MovieViewModel
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Synopsis { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Rating { get; set; }
+
+        public int DurationInMinutes { get; set; }
+
+        public string Genre { get; set; }
+
+        public string TrailerUrl { get; set; }
+
+    }
+}
