ab6777d0046adb20776e5c0d577d04b36bc82ba8
sebastianr213@gmail.com <sebastianr213@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-23 00:50:30 +0000

    [PT-122 Implementar endpoint para exponer lista de películas en cartelera (servidor)] Initial Approach

---
+++ src/PhoneTicket.Web/PhoneTicket.Web/Controllers/Api/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿namespace PhoneTicket.Web.Controllers.Api
+{
+    
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Http;
+
+    using PhoneTicket.Web.Services;
+    using PhoneTicket.Web.ViewModels;
+    using System.Threading.Tasks;
+
+    [Authorize]
+    [RoutePrefix("api/movies")]
+    public class MoviesController : ApiController
+    {
+        private readonly IMovieService movieService;
+
+        public MoviesController(IMovieService movieService)
+        {
+            this.movieService = movieService;
+        }
+
+        [HttpPost("")]
+        public async Task<IEnumerable<ListMovieViewModel>> Get()
+        {
+            var movies = await this.movieService.GetMovies();
+
+            return movies.Select(m => ListMovieViewModel.FromUser(m));
+        }
+    }
+}
