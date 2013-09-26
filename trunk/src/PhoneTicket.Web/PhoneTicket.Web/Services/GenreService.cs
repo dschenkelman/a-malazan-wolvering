064c4a5a78dd539eda331ed09051082ad057cad0
sebastianr213@gmail.com <sebastianr213@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-26 20:34:12 +0000

    [PT-129] Added Dropdown lists for Genre and Rating

---
+++ trunk/src/PhoneTicket.Web/PhoneTicket.Web/Services/GenreService.cs
@@ -0,0 +1,57 @@
+﻿namespace PhoneTicket.Web.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
+    
+
+    using PhoneTicket.Web.Data;
+    using PhoneTicket.Web.Models;
+
+    public class GenreService : IGenreService, IDisposable
+    {
+        private PhoneTicketContext db;
+
+        public GenreService(PhoneTicketContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetGenreListAsync(int? ID)
+        {
+            var genres = await this.db.Genres.ToListAsync();
+
+            return new[] { new SelectListItem { Text = "Seleccionar uno" } }.Concat(
+                   from g in genres
+                   orderby g.Name
+                   select new SelectListItem
+                   {
+                       Text = g.Name,
+                       Value = g.Id.ToString(),
+                       Selected = (g.Id == ID)
+                   });
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.db != null)
+                {
+                    this.db.Dispose();
+                    this.db = null;
+                }
+            }
+        }
+    }
+}
---
+++ trunk/src/PhoneTicket.Web/PhoneTicket.Web/Services/RatingService.cs
@@ -0,0 +1,56 @@
+﻿namespace PhoneTicket.Web.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using PhoneTicket.Web.Data;
+    using PhoneTicket.Web.Models;
+
+    public class RatingService : IRatingService, IDisposable
+    {
+        private PhoneTicketContext db;
+
+        public RatingService(PhoneTicketContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetRatingListAsync(int? ID)
+        {
+            var ratings = await this.db.Ratings.ToListAsync();
+
+            return new[] { new SelectListItem { Text = "Seleccionar uno" } }.Concat(
+                   from r in ratings
+                   orderby r.Description
+                   select new SelectListItem
+                   {
+                       Text = r.Description,
+                       Value = r.Id.ToString(),
+                       Selected = (r.Id == ID)
+                   });
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.db != null)
+                {
+                    this.db.Dispose();
+                    this.db = null;
+                }
+            }
+        }
+    }
+}
