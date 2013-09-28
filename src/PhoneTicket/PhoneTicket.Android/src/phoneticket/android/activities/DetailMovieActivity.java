b20bb8ce576abb7a9cfb71f209cde1e38cd291ff
gdfesta <gdfesta@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-28 21:42:58 +0000

    Dialog Fragment while connecting to server DetailMovieActivity and MovieListActivity

--- src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/DetailMovieActivity.java
+++ src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/DetailMovieActivity.java
@@ -3,6 +3,10 @@ package phoneticket.android.activities;
 import com.google.inject.Inject;
 
 import phoneticket.android.R;
+import phoneticket.android.activities.dialog.MessageDialogFragment;
+import phoneticket.android.activities.dialog.ProgressDialogFragment;
+import phoneticket.android.activities.dialog.MessageDialogFragment.IMessageDialogDataSource;
+import phoneticket.android.activities.dialog.ProgressDialogFragment.IProgressDialogDataSource;
 import phoneticket.android.model.IMovie;
 import phoneticket.android.services.get.IRetrieveMovieInfoService;
 import phoneticket.android.services.get.IRetrieveMovieInfoServiceDelegate;
@@ -10,14 +14,14 @@ import phoneticket.android.utils.ImageDownloader;
 import roboguice.activity.RoboFragmentActivity;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.content.Intent;
 
 public class DetailMovieActivity extends RoboFragmentActivity implements
-		IRetrieveMovieInfoServiceDelegate {
+		IRetrieveMovieInfoServiceDelegate, IMessageDialogDataSource,
+		IProgressDialogDataSource {
 
 	public static final String MovieToShowId = "DetailMovieActivity.MovieToShowId";
 
@@ -25,6 +29,11 @@ public class DetailMovieActivity extends RoboFragmentActivity implements
 	private IRetrieveMovieInfoService service;
 
 	private IMovie movie;
+
+	private String messageDialogBody;
+	private String messageDialogTitle;
+
+	private ProgressDialogFragment progressDialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +43,7 @@ public class DetailMovieActivity extends RoboFragmentActivity implements
 		int movieId = getIntent().getIntExtra(
 				DetailMovieActivity.MovieToShowId, -1);
 
+		showProgressDialog();
 		service.retrieveMovieInfo(this, movieId);
 	}
 
@@ -80,14 +90,42 @@ public class DetailMovieActivity extends RoboFragmentActivity implements
 			IMovie movie) {
 		this.movie = movie;
 		showMovie();
+		hideProgressDialog();
 	}
 
 	@Override
 	public void retrieveMovieInfoFinishWithError(
 			IRetrieveMovieInfoService service, Integer errorMessage) {
-		// TODO show a error diaog. when the dialog
-		int movieId = getIntent().getIntExtra(
-				DetailMovieActivity.MovieToShowId, -1);
-		Log.d("PhoneTicket", "Error retrieving movie info with id: " + movieId);
+		hideProgressDialog();
+		messageDialogBody = "No se pudo conectar con el servidor";
+		messageDialogTitle = "Error";
+		MessageDialogFragment dialog = new MessageDialogFragment();
+		dialog.show(getSupportFragmentManager(), "dialog.error");
 	}
+
+	@Override
+	public String getMessageTitle() {
+		return messageDialogTitle;
+	}
+
+	private void showProgressDialog() {
+		if (null == progressDialog)
+			progressDialog = new ProgressDialogFragment();
+		progressDialog.show(getSupportFragmentManager(), "dialog.progress");
+	}
+
+	private void hideProgressDialog() {
+		progressDialog.dismiss();
+	}
+
+	@Override
+	public String getProgressMessageTitle() {
+		return "Buscando Lista de Películas";
+	}
+
+	@Override
+	public String getMessage() {
+		return messageDialogBody;
+	}
+
 }
