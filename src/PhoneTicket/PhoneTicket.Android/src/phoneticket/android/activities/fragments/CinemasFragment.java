e5ced5dff8b407bc8bba00e36cf42994b733264a
servetto.matias@gmail.com <servetto.matias@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-28 19:46:22 +0000

    [Fragments] A MasterActivity using fragments for each menu on the ribbon menu

---
+++ src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/fragments/CinemasFragment.java
@@ -0,0 +1,17 @@
+package phoneticket.android.activities.fragments;
+
+import phoneticket.android.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CinemasFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_cinemas, container, false);
+	}
+}
