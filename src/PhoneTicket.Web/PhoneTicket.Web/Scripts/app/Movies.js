9f6ec9634f15408ad5f1ed4fc9af4507de3aff1c
sebastianr213@gmail.com <sebastianr213@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-28 21:29:30 +0000

    [PT-130 Implementar alta película en el controlador (incluye subir imágen)] Refresh button for movie image display

---
+++ src/PhoneTicket.Web/PhoneTicket.Web/Scripts/app/Movies.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    function createMovieController(textBox,imageBox) {
+        
+        function updateImageSource() {
+            var urlSrc = textBox.val();
+            imageBox.attr("src", urlSrc);
+        }
+
+        return {
+            updateImage: updateImageSource
+        };
+    }
+
+    $(document).ready(function () {
+        var textBox = $("#ImageUrl");
+        var imageBox = $("#MovieImg");
+        var movieController = createMovieController(textBox,imageBox);
+        
+        $("#refreshImage").on("click", movieController.updateImage);
+    });
+
+   
+
+}());
