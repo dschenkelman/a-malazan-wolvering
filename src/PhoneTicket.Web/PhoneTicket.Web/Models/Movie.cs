4c452da5c822232dde6cac79ad99b65864ac602b
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-09-29 04:19:42 +0000

    Item #129 & #143. Added validation for Add and Edit views.

--- src/PhoneTicket.Web/PhoneTicket.Web/Models/Movie.cs
+++ src/PhoneTicket.Web/PhoneTicket.Web/Models/Movie.cs
@@ -1,29 +1,48 @@
 ﻿namespace PhoneTicket.Web.Models
 {
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+
+    using PhoneTicket.Web.Properties;
 
     public class Movie
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Resources), ErrorMessage = null)]
+        [DisplayName("Título")]
         public string Title { get; set; }
-                
+
+        [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Resources), ErrorMessage = null)]
+        [DisplayName("Sinopsis")]
         public string Synopsis { get; set; }
 
+        [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Resources), ErrorMessage = null)]
+        [Url(ErrorMessageResourceName = "UrlField", ErrorMessageResourceType = typeof(Resources), ErrorMessage = null)]
+        [DisplayName("Url de imágen")]
         public string ImageUrl { get; set; }
 
+        [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Resources), ErrorMessage = null)]
+        [Url(ErrorMessageResourceName = "UrlField", ErrorMessageResourceType = typeof(Resources), ErrorMessage = null)]
+        [DisplayName("Url de trailer")]
         public string TrailerUrl { get; set; }
 
+        [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Resources), ErrorMessage = null)]
+        [Range(1, int.MaxValue, ErrorMessage = "Ingrese un número entero de minutos mayor a uno.")]
+        [DisplayName("Duración (en minutos)")]
         public int DurationInMinutes { get; set; }
 
         [ForeignKey("Genre")]
         public int GenreId { get; set; }
 
+        [DisplayName("Género")]
         public virtual Genre Genre { get; set; }
 
         [ForeignKey("Rating")]
         public int RatingId { get; set; }
 
+        [DisplayName("Clasificación")]
         public virtual Rating Rating { get; set; }
     }
 }
