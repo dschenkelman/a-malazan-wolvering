ce36c3b1322c4ba58834f4cb97fe20184981d65f
servetto.matias@gmail.com <servetto.matias@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-10-05 17:58:53 +0000

    [PT-125] accordeon with fixed height

--- trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/services/get/IRetrieveMovieFunctionsServiceDelegate.java
+++ trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/services/get/IRetrieveMovieFunctionsServiceDelegate.java
@@ -1,11 +1,13 @@
 package phoneticket.android.services.get;
 
+import java.util.Collection;
+
 import phoneticket.android.model.IMovieFunctions;
 
 public interface IRetrieveMovieFunctionsServiceDelegate {
 
 	void retrieveMovieFunctionsFinish(IRetrieveMovieFunctionsService service,
-			IMovieFunctions movieFunctions);
+			Collection<IMovieFunctions> movieFunctions);
 
 	void retrieveMovieFunctionsFinishWithError(
 			IRetrieveMovieFunctionsService service, Integer errorCode);
