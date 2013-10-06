493f1ef2f919c352fa9505f4d18af215b3a22c20
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-10-06 20:14:51 +0000

    Item #152. Initial view design

--- trunk/src/PhoneTicket.Web/PhoneTicket.Web/Scripts/app/Shows.js
+++ trunk/src/PhoneTicket.Web/PhoneTicket.Web/Scripts/app/Shows.js
@@ -1,5 +1,5 @@
 ﻿(function() {
-    $(document).ready(function () {
+    $(document).ready(function() {
         var datePickerConfig = {
             changeMonth: true,
             changeYear: true,
@@ -7,8 +7,44 @@
             gotoCurrent: true
         };
 
+        var hourSpinnerConfig = { min: 0, max: 23, numberFormat: "n" };
+
+        var minutesSpinnerConfig = { min: 0, max: 60, numberFormat: "n", page: 10 };
+
+        var timeAndRoomTemplate = "<div class=\"row\">\
+                            <div class =\"col-md-6\">\
+                                <label>Hora:</label>\
+                                <input class=\"hour spinner\" type=\"text\"/>\
+                                <span>:</span>\
+                                <input class=\"minutes spinner\" type=\"text\"/>\
+                            </div>\
+                            <div class=\"col-md-6\">\
+                                <label>Sala:</label>\
+                                <select class=\"rooms\">\
+                                    <option>Sala 1</option>\
+                                    <option>Sala 2</option>\
+                                </select>\
+                            </div>\
+                        </div>";
+
+        var timesPanel;
+
         $("#beginDate").datepicker(datePickerConfig);
         $("#endDate").datepicker(datePickerConfig);
-        $("#price").spinner({min: 0, numberFormat: "C"});
+        $("#price").spinner({ min: 0, numberFormat: "C" });
+        $(".hour").spinner(hourSpinnerConfig);
+        $(".minutes").spinner(minutesSpinnerConfig);
+
+        timesPanel = $("#shows");
+
+        $("#add").click(function() {
+            timesPanel.append(timeAndRoomTemplate);
+            timesPanel.find(".hour:last").spinner(hourSpinnerConfig);
+            timesPanel.find(".minutes:last").spinner(minutesSpinnerConfig);
+        });
+        
+        $("#remove").click(function () {
+            timesPanel.find("div.row:last").remove();
+        });
     });
-})()
+})();
