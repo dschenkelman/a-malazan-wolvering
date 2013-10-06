9d65e50a758908a98d983038a107b6a25686f578
servetto.matias@gmail.com <servetto.matias@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-10-06 18:48:52 +0000

    [PT] MovieListFragment maintains the state when you go back from the detail movie

--- trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/fragments/MovieListFragment.java
+++ trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/fragments/MovieListFragment.java
@@ -9,9 +9,11 @@ import com.google.inject.Inject;
 import phoneticket.android.R;
 import phoneticket.android.adapter.ImageAdapter;
 import phoneticket.android.model.IMovieListItem;
+import phoneticket.android.model.MovieListItem;
 import phoneticket.android.services.get.IRetrieveMovieListService;
 import phoneticket.android.services.get.IRetrieveMovieListServiceDelegate;
 import roboguice.fragment.RoboFragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,12 +28,15 @@ import android.widget.GridView;
 public class MovieListFragment extends RoboFragment implements
 		IRetrieveMovieListServiceDelegate {
 
+	private static final String STATE_MOVIELIST_STREAM = "state.movielist.stream";
+
 	@Inject
 	private IRetrieveMovieListService movieListService;
 
 	private IFragmentChange activity;
 
 	private boolean ignoreServicesCallbacks;
+	private List<IMovieListItem> movies;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,51 +58,95 @@ public class MovieListFragment extends RoboFragment implements
 	}
 
 	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
-		movieListService.retrieveMovieList(this);
-	}
-
-	@Override
-	public void retrieveMovieListFinish(IRetrieveMovieListService service,
-			Collection<IMovieListItem> movieList) {
-		if (!ignoreServicesCallbacks) {
-			GridView gridview = (GridView) getView()
-					.findViewById(R.id.gridview);
-			List<IMovieListItem> movies = new LinkedList<IMovieListItem>();
-			movies.addAll(movieList);
-			final ImageAdapter imageAdapter = new ImageAdapter(getActivity(),
-					R.id.scaleImageView, movies);
-			gridview.setAdapter(imageAdapter);
-
-			gridview.setOnItemClickListener(new OnItemClickListener() {
-
-				public void onItemClick(AdapterView<?> parent, View v,
-						int position, long id) {
-					Bundle movieData = new Bundle();
-					movieData.putInt(DetailMovieFragment.EXTRA_MOVIE_ID,
-							imageAdapter.getItem(position).getId());
-					DetailMovieFragment detailMovieFragment = new DetailMovieFragment();
-					detailMovieFragment.setArguments(movieData);
-					activity.changeFragment(detailMovieFragment);
-
-				}
-			});
-			hideProgressLayout();
-			imageAdapter.notifyDataSetChanged();
-		}
-	}
-
-	@Override
 	public void onResume() {
 		super.onResume();
 		ignoreServicesCallbacks = false;
+
+		SharedPreferences preferences = getActivity().getPreferences(0);
+		String movielistStream = preferences.getString(STATE_MOVIELIST_STREAM,
+				"");
+		boolean recreatingState = 0 != movielistStream.length();
+
+		if (recreatingState) {
+			createMovielistView(createMovielistFromStream(movielistStream));
+		} else {
+			movieListService.retrieveMovieList(this);
+		}
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
 		ignoreServicesCallbacks = true;
+
+		if (null != movies) {
+
+			String movieListStream = "";
+			for (IMovieListItem movieListItem : movies) {
+				movieListStream += movieListItem.getId() + "#"
+						+ movieListItem.getImageURL() + "]";
+			}
+			SharedPreferences.Editor editor = getActivity().getPreferences(0)
+					.edit();
+			editor.putString(STATE_MOVIELIST_STREAM, movieListStream);
+			editor.commit();
+		}
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		SharedPreferences.Editor editor = getActivity().getPreferences(0)
+				.edit();
+		editor.remove(STATE_MOVIELIST_STREAM);
+		editor.commit();
+	}
+
+	private List<IMovieListItem> createMovielistFromStream(
+			String movielistStream) {
+		List<IMovieListItem> movielist = new LinkedList<IMovieListItem>();
+		String items[] = movielistStream.split("]");
+		for (String itemStream : items) {
+			String values[] = itemStream.split("#");
+			int id = Integer.parseInt(values[0]);
+			String imageUrl = values[1];
+			MovieListItem item = new MovieListItem(id, "", imageUrl);
+			movielist.add(item);
+		}
+		return movielist;
+	}
+
+	private void createMovielistView(Collection<IMovieListItem> movieList) {
+		GridView gridview = (GridView) getView().findViewById(R.id.gridview);
+		movies = new LinkedList<IMovieListItem>();
+		movies.addAll(movieList);
+		final ImageAdapter imageAdapter = new ImageAdapter(getActivity(),
+				R.id.scaleImageView, movies);
+		gridview.setAdapter(imageAdapter);
+
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
+				Bundle movieData = new Bundle();
+				movieData.putInt(DetailMovieFragment.EXTRA_MOVIE_ID,
+						imageAdapter.getItem(position).getId());
+				DetailMovieFragment detailMovieFragment = new DetailMovieFragment();
+				detailMovieFragment.setArguments(movieData);
+				activity.changeFragment(detailMovieFragment);
+
+			}
+		});
+		hideProgressLayout();
+		imageAdapter.notifyDataSetChanged();
+	}
+
+	@Override
+	public void retrieveMovieListFinish(IRetrieveMovieListService service,
+			Collection<IMovieListItem> movieList) {
+		if (!ignoreServicesCallbacks) {
+			createMovielistView(movieList);
+		}
 	}
 
 	@Override
