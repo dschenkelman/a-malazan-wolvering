007c32de048f0db1c0007185aba569cd4c828431
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-10-06 17:40:05 +0000

    Item #152. Initial work on create show view

---
+++ src/PhoneTicket.Web/PhoneTicket.Web/Controllers/ShowsController.cs
@@ -0,0 +1,17 @@
+﻿namespace PhoneTicket.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ShowsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ src/PhoneTicket.Web/PhoneTicket.Web/Scripts/app/Shows.js
@@ -0,0 +1,14 @@
+﻿(function() {
+    $(document).ready(function () {
+        var datePickerConfig = {
+            changeMonth: true,
+            changeYear: true,
+            dateFormat: 'yyyy-mm-dd',
+            gotoCurrent: true
+        };
+
+        $("#beginDate").datepicker(datePickerConfig);
+        $("#endDate").datepicker(datePickerConfig);
+        $("#price").spinner({min: 0, numberFormat: "C"});
+    });
+})()
