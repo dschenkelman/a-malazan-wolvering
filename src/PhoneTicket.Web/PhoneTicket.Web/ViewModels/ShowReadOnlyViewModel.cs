a448a3964f80a64f59e059f9d20a334f790fcba5
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-10-15 02:24:57 +0000

    Item #181. View done, missing unit tests

---
+++ src/PhoneTicket.Web/PhoneTicket.Web/ViewModels/ShowReadOnlyViewModel.cs
@@ -0,0 +1,43 @@
+﻿namespace PhoneTicket.Web.ViewModels
+{
+    using System;
+    using System.ComponentModel;
+
+    using PhoneTicket.Web.Models;
+
+    public class ShowReadOnlyViewModel
+    {
+        public int MovieId { get; set; }
+
+        [DisplayName("Fecha")]
+        public DateTime Date { get; set; }
+
+        [DisplayName("Horario")]
+        public string Time { get; set; }
+
+        [DisplayName("Sala")]
+        public string Room { get; set; }
+
+        [DisplayName("Complejo")]
+        public string Complex { get; set; }
+
+        [DisplayName("Película")]
+        public string Movie { get; set; }
+
+        [DisplayName("Precio")]
+        public double Price { get; set; }
+
+        public static ShowReadOnlyViewModel FromShow(Show show)
+        {
+            return new ShowReadOnlyViewModel
+                       {
+                           Price = show.Price,
+                           Date = show.Date,
+                           Time = show.Date.ToString("hh:mm"),
+                           Movie = show.Movie.Title,
+                           Room = show.Room.Name,
+                           Complex = show.Room.Complex.Name
+                       };
+        }
+    }
+}
