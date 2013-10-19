8a340b055518cbbecc4d5fb71066970df0a98705
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-10-19 16:40:12 +0000

    Item #126. Completed

---
+++ trunk/src/PhoneTicket.Web/PhoneTicket.Web/ViewModels/Api/ShowInfoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PhoneTicket.Web.ViewModels.Api
+{
+    public class ShowInfoViewModel
+    {
+        public int Id { get; set; }
+
+        public string Day { get; set; }
+
+        public string Time { get; set; }
+    }
+}
---
+++ trunk/src/PhoneTicket.Web/PhoneTicket.Web/ViewModels/Api/ShowsPerComplexViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace PhoneTicket.Web.ViewModels.Api
+{
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    public class ShowsPerComplexViewModel
+    {
+        public ShowsPerComplexViewModel()
+        {
+            this.Functions = new List<ShowInfoViewModel>();
+        }
+
+        public int CinemaId
+        {
+            get { return this.ComplexId; }
+        }
+
+        public string CinemaName
+        {
+            get { return this.ComplexName; }
+        }
+
+        public List<ShowInfoViewModel> Functions { get; set; }
+        
+        [JsonIgnore]
+        public string ComplexName { get; set; }
+
+        [JsonIgnore]
+        public int ComplexId { get; set; }
+    }
+}
