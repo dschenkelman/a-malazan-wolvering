d6d474b91cb084dece135738a2a00bc421d03bc3
servetto.matias@gmail.com <servetto.matias@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-10-27 18:26:55 +0000

    [PT-311] the view layout. mock data for the layout. my shows list with custom adapter and the resources to show them

--- trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/fragments/UserShowsFragment.java
+++ trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/activities/fragments/UserShowsFragment.java
@@ -1,5 +1,6 @@
 package phoneticket.android.activities.fragments;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import com.google.inject.Inject;
@@ -9,24 +10,39 @@ import phoneticket.android.model.IMyShow;
 import phoneticket.android.services.get.IRetrieveMyShowsService;
 import phoneticket.android.services.get.IRetrieveMyShowsServiceDelegate;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import roboguice.fragment.RoboFragment;
 
 public class UserShowsFragment extends RoboFragment implements
 		IRetrieveMyShowsServiceDelegate {
 
 	private boolean ignoreServicesCallbacks;
-	
+
 	@Inject
 	private IRetrieveMyShowsService myShowsService;
+
+	private Collection<IMyShow> myShows;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_user_shows, container, false);
+		View view = inflater.inflate(R.layout.fragment_user_shows, container,
+				false);
 		return view;
 	}
 
@@ -34,8 +50,21 @@ public class UserShowsFragment extends RoboFragment implements
 	public void onResume() {
 		super.onResume();
 		ignoreServicesCallbacks = false;
-		
-		myShowsService.retrieveMyShows(this);
+
+		if (shouldRetrieveMyShows()) {
+			onRetrieveMyShowsAction();
+		} else {
+			createMyShowsLayout();
+			showMyShowsListLayout();
+		}
+
+		((Button) getView().findViewById(R.id.reloadDataButton))
+				.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						onRetrieveMyShowsAction();
+					}
+				});
 	}
 
 	@Override
@@ -49,11 +78,84 @@ public class UserShowsFragment extends RoboFragment implements
 		super.onAttach(activity);
 	}
 
+	private Collection<IMyShow> getMyShows() {
+		Collection<IMyShow> myShows = new ArrayList<IMyShow>();
+		// TODO load shows from disk
+		return myShows;
+	}
+
+	private boolean shouldRetrieveMyShows() {
+		Collection<IMyShow> myShows = getMyShows();
+		return (null == myShows) || (0 == myShows.size());
+	}
+
+	protected void onRetrieveMyShowsAction() {
+		showLoadingLayout();
+		myShowsService.retrieveMyShows(this);
+	}
+
+	private void showMyShowsListLayout() {
+		layoutVisibility(LinearLayout.GONE, RelativeLayout.GONE,
+				ListView.VISIBLE);
+	}
+
+	private void showLoadingLayout() {
+		layoutVisibility(LinearLayout.GONE, RelativeLayout.VISIBLE,
+				ListView.GONE);
+	}
+
+	private void showErrorLayout() {
+		layoutVisibility(LinearLayout.VISIBLE, RelativeLayout.GONE,
+				ListView.GONE);
+	}
+
+	private void layoutVisibility(int erroVisibility, int loadingVisibility,
+			int myShowsVisibilty) {
+		LinearLayout errorView = (LinearLayout) getView().findViewById(
+				R.id.errorLoadingDataView);
+		RelativeLayout loadingView = (RelativeLayout) getView().findViewById(
+				R.id.loadingDataLayout);
+		ListView myShows = (ListView) getView().findViewById(
+				R.id.myShowsListView);
+
+		errorView.setVisibility(erroVisibility);
+		loadingView.setVisibility(loadingVisibility);
+		myShows.setVisibility(myShowsVisibilty);
+	}
+
+	private void createMyShowsLayout() {
+		ListView myShowsList = (ListView) getView().findViewById(
+				R.id.myShowsListView);
+		
+		MyShowsAdapter myShowsAdapater = null;
+		if (null == myShowsList.getAdapter()) {
+			myShowsAdapater = new MyShowsAdapter(getActivity(), getId());
+
+			myShowsList.setAdapter(myShowsAdapater);
+		} else {
+			myShowsAdapater = (MyShowsAdapter) myShowsList.getAdapter();
+		}
+
+		myShowsAdapater.clear();
+		myShowsAdapater.addAll(myShows);
+		
+		myShowsList.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				Log.d("PhoneTicket", "My Show tapped: " + position
+						+ " with id " + id);
+			}
+		});
+	}
+
 	@Override
 	public void retrieveMyShowsServiceFinished(IRetrieveMyShowsService service,
 			Collection<IMyShow> myShows) {
 		if (false == ignoreServicesCallbacks) {
-			// TODO Auto-generated method stub
+			this.myShows = myShows;
+			createMyShowsLayout();
+			showMyShowsListLayout();
 		}
 	}
 
@@ -61,8 +163,58 @@ public class UserShowsFragment extends RoboFragment implements
 	public void retrieveMyShowsServiceFinishedWithError(
 			IRetrieveMyShowsService service, int errorCode) {
 		if (false == ignoreServicesCallbacks) {
-			// TODO Auto-generated method stub
+			showErrorLayout();
 		}
 	}
 
+	private class MyShowsAdapter extends ArrayAdapter<IMyShow> {
+
+		private final Context context;
+
+		public MyShowsAdapter(Context context, int resource) {
+			super(context, resource);
+			this.context = context;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return ((IMyShow) (myShows.toArray()[position])).getId();
+		}
+
+		@Override
+		public boolean hasStableIds() {
+			return true;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			IMyShow myShow = (IMyShow) (myShows.toArray()[position]);
+
+			LayoutInflater inflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View rowView = inflater.inflate(R.layout.row_user_show, parent,
+					false);
+
+			TextView moviewNameTextView = (TextView) rowView
+					.findViewById(R.id.userShowMovieName);
+			TextView showTimeTextView = (TextView) rowView
+					.findViewById(R.id.userShowShowTime);
+			TextView addressTextView = (TextView) rowView
+					.findViewById(R.id.userShowCinemaAddress);
+			ImageView imageView = (ImageView) rowView
+					.findViewById(R.id.userShowIcon);
+
+			moviewNameTextView.setText(myShow.getMovieName());
+			showTimeTextView.setText(myShow.getShowTime());
+			addressTextView.setText(myShow.getComplexAddress());
+
+			if (myShow.isBought()) {
+				imageView.setImageResource(R.drawable.ic_user_show_bought);
+			} else {
+				imageView.setImageResource(R.drawable.ic_user_show_reserved);
+			}
+
+			return rowView;
+		}
+	}
 }
--- trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/model/IMyShow.java
+++ trunk/src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/model/IMyShow.java
@@ -2,4 +2,14 @@ package phoneticket.android.model;
 
 public interface IMyShow {
 
+	int getId();
+
+	boolean isBought();
+
+	String getMovieName();
+
+	String getShowTime();
+
+	String getComplexAddress();
+
 }
