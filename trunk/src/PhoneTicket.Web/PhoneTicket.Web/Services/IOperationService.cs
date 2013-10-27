c31ea4c88ce14391f8f7420bac91f181aa23fed7
sebastianr213@gmail.com <sebastianr213@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-10-27 20:23:59 +0000

    [PT-301][PT-302] User's Reservation & Purchases Operations. Missing Tests

---
+++ trunk/src/PhoneTicket.Web/PhoneTicket.Web/Services/IOperationService.cs
@@ -0,0 +1,16 @@
+﻿namespace PhoneTicket.Web.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using PhoneTicket.Web.Models;
+
+    public interface IOperationService
+    {
+        Task<IEnumerable<Operation>> GetAsync(Expression<Func<Operation, bool>> filter);
+    }
+}
---
+++ trunk/src/PhoneTicket.Web/PhoneTicket.Web/ViewModels/Api/UserOperationsViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace PhoneTicket.Web.ViewModels.Api
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class UserOperationsViewModel
+    {
+        public int Id { get; set; }
+        
+        public bool IsBought { get; set; }
+
+        public string MovieTitle { get; set; }
+
+        public string ShowDateAndTime { get; set; }
+
+        public string ComplexAddress { get; set; }
+    }
+}
