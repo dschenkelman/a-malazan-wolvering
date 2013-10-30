333b8161907f60eb8d487860d5be751ef81f82d1
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-10-30 17:20:15 +0000

    Item #261. Functionality should be done. Still to do tests.

---
+++ src/PhoneTicket.Web/PhoneTicket.Web/Controllers/Api/BaseOperationsController.cs
@@ -0,0 +1,104 @@
+﻿namespace PhoneTicket.Web.Controllers.Api
+{
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Mail;
+    using System.Net.Mime;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+
+    using MessagingToolkit.QRCode.Codec;
+
+    using PhoneTicket.Web.Helpers;
+    using PhoneTicket.Web.Models;
+    using PhoneTicket.Web.Services;
+    using PhoneTicket.Web.Templates;
+    using PhoneTicket.Web.ViewModels.Api;
+
+    public abstract class BaseOperationsController : ApiController
+    {
+        private readonly IUserService userService;
+
+        private readonly IEmailService emailService;
+
+        private readonly IOccupiedSeatsService occupiedSeatsService;
+
+        protected BaseOperationsController(
+            IOperationService operationService,
+            IOccupiedSeatsService occupiedSeatsService,
+            IUserService userService,
+            IEmailService emailService,
+            IShowService showService)
+        {
+            this.OperationService = operationService;
+
+            this.occupiedSeatsService = occupiedSeatsService;
+
+            this.userService = userService;
+            this.emailService = emailService;
+            this.ShowService = showService;
+        }
+
+        protected IShowService ShowService { get; private set; }
+
+        protected IOperationService OperationService { get; private set; }
+
+        protected async Task<HttpResponseMessage> NewOperation(NewOperationViewModel newOperationViewModel, OperationType type)
+        {
+            var operations = await this.OperationService.GetAsync(o => o.ShowId == newOperationViewModel.ShowId);
+
+            var occupiedSeats = operations.SelectMany(op => op.OccupiedSeats);
+
+            if (AvailableSeatsHelper.ValidateSeats(newOperationViewModel.ArmChairs, occupiedSeats))
+            {
+                var user = await this.userService.GetUserAsync(Thread.CurrentPrincipal.Identity.Name);
+
+                var operation = newOperationViewModel.ToOperation(user.Id, type);
+
+                var operationId = await this.OperationService.CreateAsync(operation);
+
+                foreach (ArmChairViewModel wantedSeat in newOperationViewModel.ArmChairs)
+                {
+                    var newSeat = new OccupiedSeat
+                                      {
+                                          OperationId = operationId,
+                                          Row = wantedSeat.Row,
+                                          Column = wantedSeat.Column
+                                      };
+
+                    await this.occupiedSeatsService.CreateAsync(newSeat);
+                }
+
+                var show = await this.ShowService.GetAsync(operation.ShowId);
+
+                var template = new OperationEmailTemplate(user, operation, show);
+
+                var message = this.emailService.CreateMessage(
+                    "[CinemAR] Confirmación de operación", template.TransformText(), user.EmailAddress);
+
+                var encoder = new QRCodeEncoder();
+                using (var bitmap = encoder.Encode(operation.Number.ToString()))
+                {
+                    using (var stream = new MemoryStream())
+                    {
+                        bitmap.Save(stream, ImageFormat.Bmp);
+
+                        stream.Seek(0, SeekOrigin.Begin);
+
+                        message.Attachments.Add(new Attachment(stream, new ContentType("image/bmp") { Name = "CodigoQR" }));
+
+                        await this.emailService.SendAsync(message);
+                    }
+                }
+
+                return new HttpResponseMessage(HttpStatusCode.Created);
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.Conflict);
+        }
+    }
+}
--- src/PhoneTicket.Web/PhoneTicket.Web/Controllers/Api/ReservationsController.cs
+++ src/PhoneTicket.Web/PhoneTicket.Web/Controllers/Api/ReservationsController.cs
@@ -1,128 +1,45 @@
 ﻿namespace PhoneTicket.Web.Controllers.Api
 {
     using System;
-    using System.Collections.Generic;
-    using System.Drawing.Imaging;
-    using System.IO;
-    using System.Linq;
+    using System.Net;
     using System.Net.Http;
-    using System.Net.Mail;
-    using System.Net.Mime;
-    using System.Threading;
     using System.Threading.Tasks;
-    using System.Web;
     using System.Web.Http;
-
-    using MessagingToolkit.QRCode.Codec;
 
     using PhoneTicket.Web.Models;
     using PhoneTicket.Web.Services;
-    using PhoneTicket.Web.Templates;
     using PhoneTicket.Web.ViewModels.Api;
-    using System.Net;
-    using PhoneTicket.Web.Helpers;
 
     [RoutePrefix("api/reservations")]
-    public class ReservationsController : ApiController
+    public class ReservationsController : BaseOperationsController
     {
-        private readonly IUserService userService;
-
-        private readonly IEmailService emailService;
-
-        private readonly IShowService showService;
-
-        private readonly IOperationService operationService;
-
-        private readonly IOccupiedSeatsService occupiedSeatsService;
-
         public ReservationsController(
             IOperationService operationService,
             IOccupiedSeatsService occupiedSeatsService,
             IUserService userService,
             IEmailService emailService,
-            IShowService showService)
+            IShowService showService) 
+            : base(operationService, occupiedSeatsService, userService, emailService, showService)
         {
-            this.operationService = operationService;
-
-            this.occupiedSeatsService = occupiedSeatsService;
-
-            this.userService = userService;
-            this.emailService = emailService;
-            this.showService = showService;
         }
 
         [Authorize]
         [HttpPost("")]
         public async Task<HttpResponseMessage> NewReservation(NewOperationViewModel newOperationViewModel)
         {
-            var operations = await this.operationService.GetAsync(o => o.ShowId == newOperationViewModel.ShowId);
-
-            var occupiedSeats = operations.SelectMany(op => op.OccupiedSeats);
-
-            if (AvailableSeatsHelper.ValidateSeats(newOperationViewModel.ArmChairs, occupiedSeats))
-            {
-                var user = await this.userService.GetUserAsync(Thread.CurrentPrincipal.Identity.Name);
-
-                var operation = new Operation
-                                    {
-                                        UserId = user.Id,
-                                        Date = DateTimeHelpers.DateTimeInArgentina,
-                                        ShowId = newOperationViewModel.ShowId,
-                                        Type = OperationType.Reservation
-                                    };
-
-                var operationId = await this.operationService.CreateAsync(operation);
-
-                foreach (ArmChairViewModel wantedSeat in newOperationViewModel.ArmChairs)
-                {
-                    var newSeat = new OccupiedSeat
-                                      {
-                                          OperationId = operationId,
-                                          Row = wantedSeat.Row,
-                                          Column = wantedSeat.Column
-                                      };
-
-                    await this.occupiedSeatsService.CreateAsync(newSeat);
-                }
-
-                var show = await this.showService.GetAsync(operation.ShowId);
-
-                var template = new OperationEmailTemplate(user, operation, show);
-
-                var message = this.emailService.CreateMessage(
-                    "[CinemAR] Confirmación de operación", template.TransformText(), user.EmailAddress);
-
-                var encoder = new QRCodeEncoder();
-                using (var bitmap = encoder.Encode(operation.Number.ToString()))
-                {
-                    using (var stream = new MemoryStream())
-                    {
-                        bitmap.Save(stream, ImageFormat.Bmp);
-
-                        stream.Seek(0, SeekOrigin.Begin);
-
-                        message.Attachments.Add(new Attachment(stream, new ContentType("image/bmp"){ Name = "CodigoQR" }));
-
-                        await this.emailService.SendAsync(message);
-                    }
-                }
-
-                return new HttpResponseMessage(HttpStatusCode.Created);
-            }
-
-            return new HttpResponseMessage(HttpStatusCode.Conflict);
+            return await this.NewOperation(newOperationViewModel, OperationType.Reservation);
         }
 
         [Authorize]
         [HttpGet("{id}/cancel")]
         public async Task<HttpResponseMessage> CancelReservation(Guid id)
         {
-            var showId = (await this.operationService.GetAsync(id)).ShowId;
+            var showId = (await this.OperationService.GetAsync(id)).ShowId;
             
             // By cascade, it also deletes occupied seats and discounts referenced to the operation.
-            await this.operationService.DeleteAsync(id);
+            await this.OperationService.DeleteAsync(id);
 
-            await this.showService.ManageAvailability(showId);
+            await this.ShowService.ManageAvailability(showId);
 
             return new HttpResponseMessage(HttpStatusCode.OK);
         }
