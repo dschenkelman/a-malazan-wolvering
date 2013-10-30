8c8f719a4059d4aef3198f50e27a175d0e60cbcd
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-10-30 17:20:15 +0000

    Item #261. Functionality should be done. Still to do tests.

---
+++ trunk/src/PhoneTicket.Web/PhoneTicket.Web/Controllers/Api/PurchasesController.cs
@@ -0,0 +1,30 @@
+﻿namespace PhoneTicket.Web.Controllers.Api
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+
+    using PhoneTicket.Web.Models;
+    using PhoneTicket.Web.Services;
+    using PhoneTicket.Web.ViewModels.Api;
+
+    public class PurchasesController : BaseOperationsController
+    {
+        public PurchasesController(
+            IOperationService operationService,
+            IOccupiedSeatsService occupiedSeatsService,
+            IUserService userService,
+            IEmailService emailService,
+            IShowService showService) 
+            : base(operationService, occupiedSeatsService, userService, emailService, showService)
+        {
+        }
+
+        [Authorize]
+        [HttpPost("")]
+        public async Task<HttpResponseMessage> NewPurchase(NewOperationViewModel newOperationViewModel)
+        {
+            return await this.NewOperation(newOperationViewModel, OperationType.PurchaseWithReservation);
+        }
+    }
+}
--- trunk/src/PhoneTicket.Web/PhoneTicket.Web/ViewModels/Api/NewOperationViewModel.cs
+++ trunk/src/PhoneTicket.Web/PhoneTicket.Web/ViewModels/Api/NewOperationViewModel.cs
@@ -2,15 +2,43 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Web;
+
+    using PhoneTicket.Web.Helpers;
+    using PhoneTicket.Web.Models;
 
     public class NewOperationViewModel
     {
-
         public int ShowId { get; set; }
 
         public List<ArmChairViewModel> ArmChairs { get; set; }
 
+        public string CreditCardNumber { get; set; }
+
+        public string CreditCardSecurityCode { get; set; }
+
+        public DateTime? CreditCardExpirationDate { get; set; }
+
+        public int? CreditCardCompanyId { get; set; }
+
+        public Operation ToOperation(int userId, OperationType type)
+        {
+            var operation = new Operation
+            {
+                UserId = userId,
+                Date = DateTimeHelpers.DateTimeInArgentina,
+                ShowId = this.ShowId,
+                Type = type,
+            };
+
+            if (type != OperationType.Reservation)
+            {
+                operation.CreditCardNumber = this.CreditCardNumber;
+                operation.CreditCardSecurityCode = this.CreditCardSecurityCode;
+                operation.CreditCardExpirationDate = this.CreditCardExpirationDate;
+                operation.CreditCardCompanyId = this.CreditCardCompanyId;
+            }
+
+            return operation;
+        }
     }
 }
