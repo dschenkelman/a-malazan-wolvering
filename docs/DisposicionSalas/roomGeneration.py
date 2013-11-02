13451f7ff9b5d9f371b4df675e66405b1dad5ed9
sebastianr213@gmail.com <sebastianr213@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-11-02 19:36:55 +0000

    # Added Room Styles and Room Generation Script

---
+++ docs/DisposicionSalas/roomGeneration.py
@@ -0,0 +1,7 @@
+print "<Sala>"
+for i in range(1,18):
+    print "\t<Fila numero=\""+str(i)+"\" desde=\"1\" hasta=\"4\"/>"
+    print "\t<Fila numero=\""+str(i)+"\" desde=\"6\" hasta=\"19\"/>"
+    print "\t<Fila numero=\""+str(i)+"\" desde=\"21\" hasta=\"22\"/>"
+    print ""
+print "</Sala>"
