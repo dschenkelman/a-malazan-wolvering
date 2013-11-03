53f4aadfbc361dc1138391caefd7061fbbc68d02
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-11-03 01:24:33 +0000

    Reporte 3 in progress

--- src/PhoneTicket.Web/PhoneTicket.Web/Controllers/ReportsController.cs
+++ src/PhoneTicket.Web/PhoneTicket.Web/Controllers/ReportsController.cs
@@ -1,11 +1,17 @@
 ﻿namespace PhoneTicket.Web.Controllers
 {
+    using System;
+    using System.Drawing;
+    using System.IO;
     using System.Threading.Tasks;
     using System.Web.Mvc;
+    using System.Web.UI.DataVisualization.Charting;
 
     using PhoneTicket.Web.Handlers;
     using PhoneTicket.Web.Services;
     using PhoneTicket.Web.ViewModels;
+
+    using Chart = System.Web.Helpers.Chart;
 
     [Authorize]
     [RequireSsl]
@@ -53,5 +59,41 @@
             return viewModel;
         }
 
+        public Task<ActionResult> SalesPerMoviePdf()
+        {
+            var id = Guid.NewGuid();
+            var chartRelativePath = string.Format("~/Images/{0}.png", id);
+            var absolutePath = HttpContext.Server.MapPath(chartRelativePath);
+            var chart = new System.Web.UI.DataVisualization.Charting.Chart();
+
+            chart.ChartAreas.Add(new ChartArea());
+
+            chart.Series.Add(new Series("Data"));
+            chart.Series["Data"].ChartType = SeriesChartType.Pie;
+            chart.Series["Data"]["PieLabelStyle"] = "Inside";
+            chart.Series["Data"].Font = new System.Drawing.Font("Trebuchet MS", 16, System.Drawing.FontStyle.Regular);
+            chart.Series["Data"]["PieLineColor"] = "Black";
+            chart.Series["Data"].Points.DataBindXY(
+                new [] { "Dragon Ball Z", "El Conjuro", "Corazon de Leon" },
+                new [] { 200, 500, 300 });
+
+            chart.Legends.Add("Legend");
+            chart.Series["Data"].Label = "#VAL";
+            chart.Series["Data"].LegendText = "#VALX";
+            chart.Legends[0].Enabled = true;
+            chart.Width = 800;
+            chart.Height = 450;
+
+            using (var stream = new FileStream(absolutePath, FileMode.Create))
+            {
+                chart.SaveImage(stream, ChartImageFormat.Png);
+            }
+
+            var result = this.ViewPdf(new SalesPerMoviePdfViewModel { ChartRelativePath = absolutePath});
+
+            System.IO.File.Delete(absolutePath);
+
+            return Task.FromResult(result);
+        }
     }
 }
