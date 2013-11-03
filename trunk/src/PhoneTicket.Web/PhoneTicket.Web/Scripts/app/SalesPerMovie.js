75faa05df0666571ca6e99f1b28bedab0e4dff2f
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-11-03 18:27:34 +0000

    Reporte 3 with dates working. Still need to do complexes

---
+++ trunk/src/PhoneTicket.Web/PhoneTicket.Web/Scripts/app/SalesPerMovie.js
@@ -0,0 +1,37 @@
+﻿(function () {
+    var config = {
+        datePicker: { changeMonth: true, changeYear: true, dateFormat: 'yy-mm-dd', gotoCurrent: true }
+    };
+
+    function createView() {
+        var $form = $("#search");
+
+        $("#From").datepicker(config.datePicker);
+        $("#To").datepicker(config.datePicker);
+        $("#pdfView").click(function () {
+            var originalValue = $form.prop("action");
+
+            var newValue = originalValue.indexOf("Pdf") !== -1 ?
+                originalValue :
+                originalValue.replace("SalesPerMovie", "SalesPerMoviePdf");
+
+            $form.prop("action", newValue);
+
+            $form.submit();
+        });
+        
+        $("#generate").click(function () {
+            var originalValue = $form.prop("action");
+
+            var newValue = originalValue.indexOf("Pdf") !== -1 ?
+                originalValue.replace("Pdf", "") :
+                originalValue;
+
+            $form.prop("action", newValue);
+        });
+    }
+
+    $(document).ready(function () {
+        createView(document);
+    });
+})();
---
+++ trunk/src/PhoneTicket.Web/PhoneTicket.Web/ViewModels/SalesPerMovieViewModel.cs
@@ -0,0 +1,61 @@
+﻿namespace PhoneTicket.Web.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
+    using Newtonsoft.Json;
+
+    using System.ComponentModel.DataAnnotations;
+
+    using PhoneTicket.Web.Properties;
+
+    public class SalesPerMovieViewModel : IValidatableObject
+    {
+        [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Resources), ErrorMessage = null)]
+        public string From { get; set; }
+
+        [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Resources), ErrorMessage = null)]
+        public string To { get; set; }
+
+        public int ComplexId { get; set; }
+
+        public IEnumerable<SelectListItem> Complexes { get; set; }
+
+        [JsonIgnore]
+        public IEnumerable<MovieSalesViewModel> MovieSales { get; set; }
+
+        [JsonIgnore]
+        public string ChartPath { get; set; }
+
+        [JsonIgnore]
+        public DateTime FromDate 
+        {
+            get
+            {
+                return DateTime.Parse(this.From);
+            } 
+        }
+
+        [JsonIgnore]
+        public DateTime ToDate
+        {
+            get
+            {
+                return DateTime.Parse(this.To);
+            }
+        }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+
+            if (this.ToDate < this.FromDate)
+            {
+                results.Add(new ValidationResult("La fecha de fin del reporte no puede ser anterior a la de comienzo.", new[] { "To", "From" }));
+            }
+
+            return results;
+        }
+    }
+}
