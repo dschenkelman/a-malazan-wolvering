ca8d36f3619fe697894a8ba96236f08309b1c15b
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-11-03 02:24:52 +0000

    Reporte 3 pdf working with real data. Still need to add filters and HTML version

--- src/PhoneTicket.Web/PhoneTicket.Web/Services/IReportService.cs
+++ src/PhoneTicket.Web/PhoneTicket.Web/Services/IReportService.cs
@@ -1,11 +1,16 @@
 ﻿namespace PhoneTicket.Web.Services
 {
+    using System;
+    using System.Collections.Generic;
     using System.Threading.Tasks;
 
     using PhoneTicket.Web.Models;
+    using PhoneTicket.Web.ViewModels;
 
     public interface IReportService
     {
         Task<ReportShowSeats> GetReportSeatsAsync(int showId);
+
+        Task<IEnumerable<MovieSalesViewModel>> GetSalesPerMovieReport(DateTime beginDate, DateTime endDate);
     }
 }
