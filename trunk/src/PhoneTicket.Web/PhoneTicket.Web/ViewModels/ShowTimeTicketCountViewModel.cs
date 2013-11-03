7c38ec2d864dacd78e130deda791a9d56f87684d
sebastianr213@gmail.com <sebastianr213@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-11-03 07:48:47 +0000

    # Report for best selling show times. No tests

---
+++ trunk/src/PhoneTicket.Web/PhoneTicket.Web/ViewModels/ShowTimeTicketCountViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace PhoneTicket.Web.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class ShowTimeTicketCountViewModel
+    {
+        public string Time { get; set; }
+
+        public int TicketCount { get; set; }
+
+        public int MovieCount { get; set; }
+    }
+}
