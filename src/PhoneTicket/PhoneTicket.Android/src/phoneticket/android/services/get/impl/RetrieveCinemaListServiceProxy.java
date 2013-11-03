33fb7945ea83228e0d3d363e4537d08376641e4c
servetto.matias@gmail.com <servetto.matias@gmail.com@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-11-03 17:03:00 +0000

    [BUG] retrieve cinemas mock class removed from proxy and visual changes on discounts row layout

--- src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/services/get/impl/RetrieveCinemaListServiceProxy.java
+++ src/PhoneTicket/PhoneTicket.Android/src/phoneticket/android/services/get/impl/RetrieveCinemaListServiceProxy.java
@@ -2,14 +2,13 @@ package phoneticket.android.services.get.impl;
 
 import phoneticket.android.services.get.IRetrieveCinemaListService;
 import phoneticket.android.services.get.IRetrieveCinemaListServiceDelegate;
-import phoneticket.android.services.get.mock.MockRetrieveCinemaListService;
 
 public class RetrieveCinemaListServiceProxy implements
 		IRetrieveCinemaListService {
 
 	@Override
 	public void retrieveCinemaList(IRetrieveCinemaListServiceDelegate delegate) {
-		(new MockRetrieveCinemaListService()).retrieveCinemaList(delegate);
+		(new RetrieveCinemaListService()).retrieveCinemaList(delegate);
 	}
 
 }
