c24cd94d32dc5276d9ca5ac3a51238e9eaaa448e
gdfesta <gdfesta@2a5ade1e-0ce3-53c1-387a-2b2623bf7fdd>
2013-11-16 15:52:09 +0000

    Tests on Detail Movie Fragment

--- trunk/src/PhoneTicket/PhoneTicket.Android/test/activities/DetailMovieTest.java
+++ trunk/src/PhoneTicket/PhoneTicket.Android/test/activities/DetailMovieTest.java
@@ -16,21 +16,29 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.shadows.ShadowIntent;
+import org.robolectric.shadows.ShadowPreferenceManager;
 
 import phoneticket.android.R;
 import phoneticket.android.activities.MasterActivity;
 import phoneticket.android.activities.fragments.DetailCinemaFragment;
 import phoneticket.android.activities.fragments.DetailMovieFragment;
+import phoneticket.android.activities.fragments.RoomFragment;
 import phoneticket.android.services.get.IRetrieveCinemaInfoService;
 import phoneticket.android.services.get.IRetrieveMovieFunctionsService;
 import phoneticket.android.services.get.IRetrieveMovieInfoService;
 import phoneticket.android.services.get.IRetrieveMovieListService;
+import phoneticket.android.services.get.IRetrieveRoomInfoService;
 import phoneticket.android.services.get.mock.MockRetrieveMovieFunctionsService;
 import phoneticket.android.services.get.mock.MockRetrieveMovieInfoService;
+import phoneticket.android.utils.UserManager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 @RunWith(RobolectricTestRunner.class)
 public class DetailMovieTest {
@@ -43,6 +51,8 @@ public class DetailMovieTest {
 	private IRetrieveMovieListService iRetrieveMovieListService;
 	@Mock
 	private IRetrieveCinemaInfoService iRetrieveCinemaInfoService;
+	@Mock
+	private IRetrieveRoomInfoService roomInfoService;
 
 	private DetailMovieFragment fragment;
 	private MasterActivity activity;
@@ -62,6 +72,7 @@ public class DetailMovieTest {
 				iRetrieveMovieListService);
 		module.addBinding(IRetrieveCinemaInfoService.class,
 				iRetrieveCinemaInfoService);
+		module.addBinding(IRetrieveRoomInfoService.class, roomInfoService);
 
 		TestModule.setUp(this, module);
 
@@ -75,6 +86,10 @@ public class DetailMovieTest {
 
 		watchTrailerButton = (Button) fragment.getView().findViewById(
 				R.id.watchTrailerButton);
+		SharedPreferences sharedPreferences = ShadowPreferenceManager
+				.getDefaultSharedPreferences(Robolectric.application
+						.getApplicationContext());
+		UserManager.initialize(sharedPreferences);
 	}
 
 	@Test
@@ -112,6 +127,48 @@ public class DetailMovieTest {
 
 		Assert.assertEquals(
 				DetailCinemaFragment.class.getCanonicalName(),
+				activity.getSupportFragmentManager()
+						.findFragmentById(R.id.fragment_container).getClass()
+						.getCanonicalName());
+	}
+
+	@Test
+	public void detailMovieChangeCallLoginActivityWhenNotLogIn() {
+		new MockRetrieveMovieInfoService().retrieveMovieInfo(fragment, 0);
+
+		new MockRetrieveMovieFunctionsService().retrieveMovieFunctions(
+				fragment, movieId);
+
+		LinearLayout button = (LinearLayout) fragment.getView().findViewById(
+				R.id.timeLinearLayout);
+
+		TextView v = (TextView) button.getChildAt(0);
+
+		v.performClick();
+
+		ShadowActivity shadowActivity = Robolectric.shadowOf(activity);
+		Intent intent = shadowActivity.getNextStartedActivity();
+		assertNotNull(intent);
+	}
+
+	@Test
+	public void detailMovieChangeChangeFragmentOnLoginIn() {
+		new MockRetrieveMovieInfoService().retrieveMovieInfo(fragment, 0);
+
+		new MockRetrieveMovieFunctionsService().retrieveMovieFunctions(
+				fragment, movieId);
+
+		LinearLayout button = (LinearLayout) fragment.getView().findViewById(
+				R.id.timeLinearLayout);
+
+		UserManager.getInstance().setCredentials("pepe", "pepe");
+		UserManager.getInstance().loginUserWithId(0, true);
+		TextView v = (TextView) button.getChildAt(0);
+
+		v.performClick();
+
+		Assert.assertEquals(
+				RoomFragment.class.getCanonicalName(),
 				activity.getSupportFragmentManager()
 						.findFragmentById(R.id.fragment_container).getClass()
 						.getCanonicalName());
